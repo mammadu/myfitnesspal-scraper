447c4658a1538dd8b14ac08b9d521979938e0d49
mammadu <mammadudiallo@gmail.com>
2021-06-02 22:19:33 -0500

    Implement basic linking and url_for Index page now has a link to the oauth page. This oauth page will eventually redirect to the google fit oauth page.

--- app.py
+++ app.py
@@ -1,11 +1,19 @@
-from flask import Flask, request, url_for, session, redirect
+from flask import Flask, request, url_for, session, redirect, render_template
 
 app = Flask(__name__)
 
 @app.route("/")
-def test_statement():
-    return "sup bitches"
+def index():
+    oauth_address = url_for('oauth')
+    return f"<a href={oauth_address}> oauth login</a>"
 
-@app.route("/yikes")
-def new_statement():
+@app.route("/oauth")
+def oauth():
     return "ohno"
+
+@app.route("/success")
+def suscess():
+    return "Successful redirection!"
+
+if __name__ == "__main__":
+    app.run(ssl_context='adhoc') #doesn't seem to make the server run using https. Use "flask run --cert=adhoc" on the command line to enable https
