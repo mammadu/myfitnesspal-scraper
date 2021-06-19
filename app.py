0e95d8dfce0338f788ae10e541390ec2f8097b97
mammadu <muhamadude@gmail.com>
2021-06-19 12:11:35 -0500

    Program can now get access code oauth route is now able to get access code from google. We can use this to get the access token to get the google fit data. Need to figure out how to access the code from the URI

--- app.py
+++ app.py
@@ -6,7 +6,7 @@ app = Flask(__name__)
 @app.route("/")
 def index():
     oauth_address = url_for('oauth')
-    return f"<a href={oauth_address}> oauth login</a>"
+    return f"<a href={oauth_address}>login</a>"
 
 @app.route("/oauth")
 def oauth():
@@ -15,13 +15,14 @@ def oauth():
     
     auth_uri = data["web"]["auth_uri"]
     client_id = data["web"]["client_id"]
-    redirect_uris = data["web"]["redirect_uris"]
-    #need to creadte redirect uri
-    print(data["web"]["project_id"])
-    return data
+    redirect_uris = data["web"]["redirect_uris"][0]
+    total_auth_uri = auth_uri + "?" + "response_type=code" + "&" + f"client_id={client_id}" + "&" + f"redirect_uri={redirect_uris}" + "&" + "scope=https://www.googleapis.com/auth/fitness.nutrition.read" + "&" + "state=1234"
+
+    # print(total_auth_uri)
+    return f"<a href={total_auth_uri}/> oauth</a>"
 
 @app.route("/success")
-def suscess():
+def success(): #need to determine how to use get code and exchange for access token
     return "Successful redirection!"
 
 if __name__ == "__main__":
