01459fd48ec729fefb862c3aa3badf3efee056b1
mammadu <mammadudiallo@gmail.com>
2021-12-10 17:16:33 -0600

    Create setup.py initial plumbing of setup.py

---
+++ setup.py
@@ -0,0 +1,18 @@
+from setuptools import setup
+
+setup(
+    name = 'myfitnesspal-scraper'
+    , version = '0.1'
+    , description = "Scrapes nutrional data from myfitnesspal and saves to various locations"
+    , url='https://github.com/mammadu/myfitnesspal-scraper'
+    , python_requires='>=3.6'
+    , install_requires = [
+        'beautifulsoup'
+        'requests'
+        'datetime'
+        'pandas'
+        'flask'
+        'getpass'
+        'lxml'
+    ]
+)
